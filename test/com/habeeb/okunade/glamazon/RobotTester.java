526744637aa5a3f2ff1da6a019591af83478ec28
Habeeb Okunade <guessy2k2@yahoo.com>
2019-02-05 16:34:55 +1100

    Implemented Test Class

---
+++ test/com/habeeb/okunade/glamazon/RobotTester.java
@@ -0,0 +1,69 @@
+package com.habeeb.okunade.glamazon;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class RobotTester {
+
+	@Test
+	void testRobot() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	void testMoveRight() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	void testMoveLeft() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	void testMoveUp() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	void testMoveDown() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	void testGetLocX() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	void testGetLocY() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	void testGetGrid() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	void testPickGift() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	void testGetGiftPicked() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	void testGetRobotInitialLocation() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	void testGetRobotCurrentLocation() {
+		fail("Not yet implemented"); // TODO
+	}
+
+}
