330ac052a81a9889bf7cd8b63d005dbb78fd34f2
Habeeb Okunade <guessy2k2@yahoo.com>
2019-02-05 20:13:03 +1100

    Test cases passed

--- test/com/habeeb/okunade/glamazon/RobotTester.java
+++ test/com/habeeb/okunade/glamazon/RobotTester.java
@@ -1,53 +1,90 @@
 package com.habeeb.okunade.glamazon;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.jupiter.api.Assertions.*;
 
-import org.junit.jupiter.api.BeforeAll;
+import java.util.Arrays;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.platform.runner.JUnitPlatform;
+import org.junit.platform.suite.api.SuiteDisplayName;
 import org.junit.runner.RunWith;
 
 import com.habeeb.okunade.glamazon.model.Robot;
 
 @RunWith(JUnitPlatform.class)
-@DisplayName("Robot Tester")
+@SuiteDisplayName("Robot Tester")
 class RobotTester {
 	
-	private static Robot robot;
+	private Robot robot;
 	
-	@BeforeAll
-	public static void beforeAll() {
-	  System.out.println("**--- Robot Tester ---**");
-	  robot = new Robot(0,0,3);
-	}
 	
 	@Test
-	@DisplayName("Robot cannot move out of the grid when at the upper-left edge")
-	  public static void testRobotCannotMoveOutOfGrid() {
-	    // Test case with the age is a numeric string
+	@DisplayName("Robot left movement")
+	  void testMoveLeft() {
+	    assertNull(robot);
+	    robot = new Robot(0,0,3);
+	    // Move robot to the left at position[0,0]
 	    robot.moveLeft();
-	    robot.moveUp();
-	    assertAll("Do many assertions.", () -> {
-	      assertNotNull(robot);
-	      assertEquals(0, robot.getLocX(), "Robot cannot move left");
-	      assertEquals(0, robot.getLocY(), "Robot cannot move up");
-	    });
+	    assertEquals("Robot cannot move left", 0, robot.getLocX());
+	    assertEquals("Robot cannot move left", 0, robot.getLocY());
+	    
+	    assertNotNull(robot);
+	    robot = new Robot(2,2,3);
+	    // Move robot to the left at position[2,2]
+	    robot.moveLeft();
+	    assertEquals("Robot can move left", 1, robot.getLocX());
+	    assertEquals("Robot can move left", 2, robot.getLocY());
 	}
 	
 	@Test
 	@DisplayName("Robot cannot move out of the grid when at the upper-left edge")
-	  public static void testRobotCanMoveInsideGrid() {
-	    // Test case with the age is a numeric string
-	    robot.moveRight();
-	    robot.moveDown();
-	    assertAll("Do many assertions.", () -> {
-	      assertNotNull(robot);
-	      assertEquals(1, robot.getLocX(), "Robot can move right");
-	      assertEquals(1, robot.getLocY(), "Robot cannot move down");
-	    });
+	  void testMoveUp() {
+	    assertNull(robot);
+	    robot = new Robot(0,0,3);
+	    // Move robot to the up at position[0,0]
+	    robot.moveUp();
+	    assertEquals("Robot cannot move up", 0, robot.getLocX());
+	    assertEquals("Robot cannot move up", 0, robot.getLocY());
 	}
 	
+	@Test
+	@DisplayName("Robot can move right of the grid when at the upper-left edge")
+	  void testMoveRight() {
+	    assertNull(robot);
+	    robot = new Robot(0,0,3);
+	    // Move robot to the right from position[0,0]
+	    robot.moveRight();
+	    assertEquals("Robot can move right", 1, robot.getLocX());
+	    assertEquals("Robot can move right", 0, robot.getLocY());
+	}
 	
+	@Test
+	@DisplayName("Robot can move down of the grid when at the upper-left edge")
+	  void testMoveDown() {
+	    assertNull(robot);
+	    robot = new Robot(0,0,3);
+	    // Move robot to the right from position[0,0]
+	    robot.moveDown();
+	    assertEquals("Robot can move down", 0, robot.getLocX());
+	    assertEquals("Robot can move down", 1, robot.getLocY());
+	}
+		
+	@Test
+	@DisplayName("Robot cannot pick more than one gift at the same location")
+	 void testPickGift() {
+	    assertNull(robot);
+	    robot = new Robot(0,0,3);
+	    // Robot cannot pick same gift twice in the same location
+	    robot.pickGift(Arrays.asList(1,2));
+	    assertEquals("Robot can pick", 1, robot.getGiftPicked().size());
+
+	    robot.pickGift(Arrays.asList(1,2));
+	    assertEquals("Robot cannot pick", 1, robot.getGiftPicked().size());
+	    
+	    robot.pickGift(Arrays.asList(1,0));
+	    assertEquals("Robot can pick", 2, robot.getGiftPicked().size());
+	}
 
 }
