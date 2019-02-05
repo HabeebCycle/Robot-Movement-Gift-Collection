c6a49e8a5e86571a870b334a0b1491afd13c2512
Habeeb Okunade <guessy2k2@yahoo.com>
2019-02-05 16:42:26 +1100

    Test Cases

--- test/com/habeeb/okunade/glamazon/RobotTester.java
+++ test/com/habeeb/okunade/glamazon/RobotTester.java
@@ -2,68 +2,12 @@ package com.habeeb.okunade.glamazon;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+@DisplayName("Robot Tester")
 class RobotTester {
 
-	@Test
-	void testRobot() {
-		fail("Not yet implemented"); // TODO
-	}
-
-	@Test
-	void testMoveRight() {
-		fail("Not yet implemented"); // TODO
-	}
-
-	@Test
-	void testMoveLeft() {
-		fail("Not yet implemented"); // TODO
-	}
-
-	@Test
-	void testMoveUp() {
-		fail("Not yet implemented"); // TODO
-	}
-
-	@Test
-	void testMoveDown() {
-		fail("Not yet implemented"); // TODO
-	}
-
-	@Test
-	void testGetLocX() {
-		fail("Not yet implemented"); // TODO
-	}
-
-	@Test
-	void testGetLocY() {
-		fail("Not yet implemented"); // TODO
-	}
-
-	@Test
-	void testGetGrid() {
-		fail("Not yet implemented"); // TODO
-	}
-
-	@Test
-	void testPickGift() {
-		fail("Not yet implemented"); // TODO
-	}
-
-	@Test
-	void testGetGiftPicked() {
-		fail("Not yet implemented"); // TODO
-	}
-
-	@Test
-	void testGetRobotInitialLocation() {
-		fail("Not yet implemented"); // TODO
-	}
-
-	@Test
-	void testGetRobotCurrentLocation() {
-		fail("Not yet implemented"); // TODO
-	}
+	
 
 }
