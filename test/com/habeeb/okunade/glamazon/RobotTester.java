0a5408d9fe31fef983a667a0ac98bdf8fabfeb84
Habeeb Okunade <guessy2k2@yahoo.com>
2019-02-05 16:55:21 +1100

    Robot Tester

--- test/com/habeeb/okunade/glamazon/RobotTester.java
+++ test/com/habeeb/okunade/glamazon/RobotTester.java
@@ -2,12 +2,18 @@ package com.habeeb.okunade.glamazon;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import org.mockito.junit.jupiter.MockitoExtension;
 
 @DisplayName("Robot Tester")
 class RobotTester {
 
-	
+	@BeforeAll
+	static void beforeAll() {
+	  System.out.println("**--- Robot Tester ---**");
+	}
 
 }
